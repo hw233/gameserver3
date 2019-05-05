254a95f5ae12d24c8c463befbb0d690ea5b06b13
87399497 <87399497@qq.com>
2019-05-05 11:19:09 +0800

    nothing

---
+++ client/library/imports/72/72f30bb5-b7b5-4670-9338-563a27f8de75.js
@@ -0,0 +1,57 @@
+"use strict";
+cc._RF.push(module, '72f30u1t7VGcJM4Vjon+N51', 'loading');
+// module/loading/script/loading.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        // 初始化所有组件
+        this.initMgr();
+    },
+    start: function start() {},
+
+
+    // update (dt) {},
+
+    initMgr: function initMgr() {
+        // 全局对象
+        cc.vv = {};
+
+        cc.vv.userMgr = '';
+    }
+});
+
+cc._RF.pop();
